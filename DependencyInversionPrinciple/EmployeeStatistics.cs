77023bc76e3a0c30faf6326ea36723cd7f29acb4
raselkobir1 <raselkobir29@gmail.com>
2022-02-21 00:01:16 +0600

    tightly couple code example DIP

---
+++ DependencyInversionPrinciple/EmployeeStatistics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversionPrinciple
+{
+    public class EmployeeStatistics  //higher lavel class
+    {
+        private readonly EmployeeManager _empManager;
+        public EmployeeStatistics(EmployeeManager empManager) 
+        {
+            _empManager = empManager;
+        }
+        public int CountFemaleManager()
+        {
+            return _empManager.Employees().Count(emp => emp.Gender.Equals(Gender.Female) && emp.Position.Equals(Position.Manager));
+        }
+    }
+}
+ 
